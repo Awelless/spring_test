f6b239a453c7c07baf90ed5f31e6dc37475981be
Awelless <hahaha.funny.yellow.dog@gmail.com>
2020-10-30 23:39:09 +0300

    nyaaan

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,30 @@
+package com.example.sweater.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NonNull
+    private String text;
+    @NonNull
+    private String tag;
+    @NonNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String filename;
+
+    public String getAuthor() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+}
